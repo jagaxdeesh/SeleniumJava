f91690c3e651071886b80c313b2260c31b61ab04
jagaxdeesh <jagaxdeesh@gmail.com>
2025-04-16 18:53:06 +0530

    Update Jeswanth GitDemo.java

--- src/launchbrowsers/GitDemo.java
+++ src/launchbrowsers/GitDemo.java
@@ -9,11 +9,11 @@ public class GitDemo {
 	WebDriver driver;
   @Test
   public void techlearn() {
-	  driver.get("https://www.techlearn.in");
+	  driver.get("https://www.techlearn.in/admin");
   }
   @BeforeTest
   public void beforeTest() {
-	  driver = new ChromeDriver();
+	  driver = new FirefoxDriver();
 	  driver.manage().window().maximize();
   }
 
