666541cfe88e7bf591a3d3bc451ea37167198506
jagaxdeesh <jagaxdeesh@gmail.com>
2025-04-16 18:50:41 +0530

    TestDemo

--- src/launchbrowsers/GitDemo.java
+++ src/launchbrowsers/GitDemo.java
@@ -1,14 +1,20 @@
 package launchbrowsers;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 
 public class GitDemo {
+	WebDriver driver;
   @Test
-  public void f() {
+  public void techlearn() {
+	  driver.get("https://www.techlearn.in");
   }
   @BeforeTest
   public void beforeTest() {
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
   }
 
 }
